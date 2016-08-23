5c7d784f6491077a9d28755713956c5f216dd041
RainLingMO <2855132411@qq.com>
2016-08-23 09:06:04 +0800

    hpw

---
+++ hpw/skin/js/public.js
@@ -0,0 +1,80 @@
+﻿/*center_swt_judge*/
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('i(0($){e b=\'2://3.4.5/s.6?7=8&9=c&d=?&1=\'+f.g();$.h(b,0(a){j(a.k=="l.m.n.o"){}p{q("r()",t)}})});',30,30,'function|_|http|chaxun|1616|net|php|type|ip|output|||json|callback|var|Math|random|getJSON|jQuery|if|Ip|114|94|159|137|else|setTimeout|showC||50000'.split('|'),0,{}))
+
+/*search*/
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('7 f(){0 a="9://g.h.i/l.m/5/6";0 b=8;0 c=4;0 d=(1.3.j-k-c)/2;0 e=(1.3.n-o-b)/2;1.p(a,"","q="+c+", r="+b+", s="+d+", t="+e)}',30,30,'var|window||screen|600|home|personal|function|900|http||||||cjsearch|www|alltobid|com|availHeight|30|gpcarquery|web|availWidth|10|open|height|width|top|left'.split('|'),0,{}))
+
+/*fiexd div*/
+$(document).ready(function(){
+	var topMain=$("#webtop").height()-50;
+	var nav=$(".menutop");
+	$(window).scroll(function(){
+		if ($(window).scrollTop()>topMain){
+			nav.addClass("scroll");
+		}else{
+			nav.removeClass("scroll");
+		}
+	});
+	var topMain2=850;
+	var nav2=$(".right2");
+	$(window).scroll(function(){
+		if ($(window).scrollTop()>topMain2){
+			nav2.addClass("scroll2");
+		}else{
+			nav2.removeClass("scroll2");
+		}
+	});
+
+})
+
+
+/*right_side*/
+
+$(document).ready(function(){
+
+	$(".side ul li").hover(function(){
+		$(this).find(".sidebox").stop().animate({"width":"124px"},200).css({"opacity":"1","filter":"Alpha(opacity=100)","background":"#FE0002"})	
+	},function(){
+		$(this).find(".sidebox").stop().animate({"width":"54px"},200).css({"opacity":"0.8","filter":"Alpha(opacity=80)","background":"#000000"})	
+	});
+	$("#eeee").hover(function(){
+		$(".erwei").fadeIn(400);
+		},function(){
+			$(".erwei").hide();
+			});
+});
+
+$(window).scroll(function(){if($(window).scrollTop()>300){$('.side').fadeIn();}else{$('.side').fadeOut();}});
+
+/*goTop*/
+function goTop(){
+	$('html,body').animate({'scrollTop':0},800);
+}
+
+function talk(){eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('0 6="i://d.f.h.j//m/n?9=a&b=c";0 4=e;0 1=g;0 5=(3.7.k-l-1)/2;0 8=(3.7.o-p-4)/2;3.q(6,"","r="+1+", s="+4+", t="+5+", u="+8);',31,31,'var|iHeight||window|iWidth|iTop|openUrl|screen|iLeft|siteid|7002041|ucid|10700061|webim|800|qiao|600|baidu|http|com|availHeight|30|im|index|availWidth|10|open|height|width|top|left'.split('|'),0,{}))}
+
+
+/*center_swt*/
+
+function showC(){
+	$("#divMenu").fadeIn(1000);
+}
+function hideC(){
+	$("#divMenu").fadeOut(800);
+	setTimeout("showC()",60000);
+}
+
+function swtalk(){
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('0 5="d://e.f.g/h/k.l?9=a";0 1=c;0 3=s;0 6=(4.7.i-j-3)/2;0 8=(4.7.m-n-1)/2;4.o(5,"","p="+3+", q="+1+", r="+6+", b="+8);',29,29,'var|iWidth||iHeight|window|openUrl|iTop|screen|iLeft|id|PWT72549571|left|800|http|pwt|zoosnet|net|LR|availHeight|30|Chatpre|aspx|availWidth|10|open|height|width|top|600'.split('|'),0,{}))}
+
+/*center_swtalk_html*/
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('1.2("<l k=\\"L\\" J=\\"I-i:-D;m:B;i:x%;z-K:M;C-y:E( 0, 0, 0) g g O;n:o !p;q:r;\\"> ");1.2("");1.2("	<s t=\\"/u/f.v\\" w=\\"#e\\">");1.2("    <h A=\\"e\\" k=\\"e\\">");1.2("        <d c=\\"b\\" a=\\"F,5,G,H\\" 8=\\"7:j();\\" 6=\\"4\\" N=\\"j();\\" 3=\\"关闭\\">");1.2("        <d c=\\"b\\" a=\\"9,P,Q,R\\" 8=\\"7:f();\\" 6=\\"4\\" 3=\\"在线咨询\\">");1.2("        <d c=\\"b\\" a=\\"S,T,U,V\\" 8=\\"7:f();\\" 6=\\"4\\" 3=\\"咨询客服\\">");1.2("    </h>");1.2("");1.2("</l>");1.2("");',58,58,'|document|writeln|title|_self||target|javascript|href||coords|rect|shape|area|MapM|swtalk|0px|map|left|hideC|id|div|top|position|fixed|rgb|406|430|28|margin|style|index|divMenu|999|onclick|35px|200|319|260|336|228|422|251'.split('|'),0,{}))
+
+/*right_talk_erweima*/
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('1.2("<5 6=\\"s\\">");1.2("	<k>");1.2("		<4><a b=\\"e:A()\\"><5 6=\\"d\\"><9 8=\\"/7/t.c\\">在线沟通</5></a></4>");1.2("		<4><a b=\\"w://x.j.D/F?v=3&o=p&q=j&r=T\\" n=\\"i\\" f=\\\'g\\\'><5 6=\\"d\\"><9 8=\\"/7/y.c\\">z咨询</5></a></4>");1.2("		<4 l=\\"B\\"><a b=\\"e:C(0);\\" 6=\\"m\\" f=\\\'g\\\'><9 8=\\"/7/E.c\\" l=\\"h\\"></a></4>");1.2("		<4><a b=\\"/G/\\" n=\\"i\\" f=\\\'g\\\'><5 6=\\"d\\"><9 8=\\"/7/H.c\\">公司地址</5></a></4>");1.2("		<4 I=\\"J:K;\\"><a b=\\"e:L();\\" 6=\\"m\\"><9 8=\\"/7/M.c\\"></a></4>");1.2("	</k>");1.2("	<5 6=\\"h\\"><9 8=\\"/7/N.O\\" P=\\"航拍网代拍车牌微信二维码\\ewm|yes'.split('|'),0,{}))
+
+
+/*left_swtalk*/
+
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('1.2("<3 d=\\"4:f%;m-4:-6;7:8;9-b:c( 0, 0, 0) 5 5 u;g-h:i;j:k !l;\\"> ");1.2("	<a n=\\"o:p();\\"><q r=\\"/s/t.e\\"></a>");1.2("</3>");1.2("");',31,31,'|document|writeln|div|left|0px|620px|top|118px|box||shadow|rgb|style|gif|50|z|index|999|position|fixed|important|margin|href|javascript|swtalk|img|src|images|online_cn|10px'.split('|'),0,{}))
+
